36b85d3ea68e0606b2b60e5325540ff581fbd6d2
Benas <atomictacodev@gmail.com>
2025-10-26 01:22:42 +0300

    Migrate database

---
+++ Shelfie/Migrations/20251025221655_LibraryAndPlacedObjectsTables.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Shelfie.Migrations
+{
+    /// <inheritdoc />
+    public partial class LibraryAndPlacedObjectsTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "app");
+
+            migrationBuilder.CreateTable(
+                name: "Libraries",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libraries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Libraries_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "identity",
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlacedObjects",
+                schema: "app",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ObjectId = table.Column<int>(type: "integer", nullable: false),
+                    PositionX = table.Column<float>(type: "real", nullable: false),
+                    PositionY = table.Column<float>(type: "real", nullable: false),
+                    Rotation = table.Column<float>(type: "real", nullable: false),
+                    LibraryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlacedObjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlacedObjects_Libraries_LibraryId",
+                        column: x => x.LibraryId,
+                        principalSchema: "app",
+                        principalTable: "Libraries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Libraries_UserId",
+                schema: "app",
+                table: "Libraries",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlacedObjects_LibraryId",
+                schema: "app",
+                table: "PlacedObjects",
+                column: "LibraryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlacedObjects",
+                schema: "app");
+
+            migrationBuilder.DropTable(
+                name: "Libraries",
+                schema: "app");
+        }
+    }
+}
