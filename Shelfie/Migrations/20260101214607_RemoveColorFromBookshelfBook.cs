0048de7be3ac50ebacd176c73b5b0656f0ae62d4
Benas <atomictacodev@gmail.com>
2026-01-02 13:37:38 +0200

    Remove color from bookshelf book

---
+++ Shelfie/Migrations/20260101214607_RemoveColorFromBookshelfBook.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shelfie.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveColorFromBookshelfBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                schema: "app",
+                table: "BookshelfBooks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                schema: "app",
+                table: "BookshelfBooks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
